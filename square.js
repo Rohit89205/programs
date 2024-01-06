4380b853c08471a8620fc964c52851e1eda69ceb
Rohit89205 <rohit892051182@gmail.com>
2024-01-06 11:57:42 +0530

    new push

--- square.js
+++ square.js
@@ -1,3 +1,24 @@
-function sq(){
-    console.log('Hell')
+function sq(n){
+    return n * n;
 }
+
+let a = 'Rohit'
+// console.log(sq(45));
+
+
+
+function squareRoot(num){
+    let sum = num / 2;
+    let temp = 0;
+
+    while(sum !== temp){
+        temp = sum;
+        sum = (num / sum + sum) / 2;
+    console.log(sum, 'sum')
+
+    }
+    // return sum
+    // return Math.sqrt(num)
+}
+
+console.log(squareRoot(64));
